874f7658fcb11ec6109b2e590774ce45d5a6abe8
kmichels <konrad.michels@me.com>
2014-02-25 15:13:15 -0800

    finally figured out the grep part

--- scraper.py
+++ scraper.py
@@ -5,10 +5,17 @@ optionsUrl = 'http://finance.yahoo.com/q/op?s=AAPL&m=2014-04'
 optionsPage = urlopen(optionsUrl)
 soup = BeautifulSoup(optionsPage)
 
+# create a list of all the table rows that have the corresponding attrs
 optionsTable = [
 	[x.text for x in y.parent.contents]
 	for y in soup.find_all('td', attrs={'class': 'yfnc_h', 'nowrap': ''})
 ]
 
+# last item in list is gibberish: kill it with fire
+del optionsTable[-1]
+
+# now clear out all the call options
+
 for item in optionsTable:
-	print item
+	if 'C' not in item[1]:
+		print item
