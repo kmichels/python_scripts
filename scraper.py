206fa6dadd82a3734579c998b384c0692b1e489b
kmichels <konrad.michels@me.com>
2014-02-26 14:36:24 -0800

    figured out how to get a vanilla stock price quote

--- scraper.py
+++ scraper.py
@@ -1,14 +1,15 @@
 from urllib import urlopen
 from bs4 import BeautifulSoup
+import ystockquote
 
 optionsUrl = 'http://finance.yahoo.com/q/op?s=AAPL&m=2014-04'
 optionsPage = urlopen(optionsUrl)
-soup = BeautifulSoup(optionsPage)
+optionssoup = BeautifulSoup(optionsPage)
 
 # create a list of all the table rows that have the corresponding attrs
 optionsTable = [
 	[x.text for x in y.parent.contents]
-	for y in soup.find_all('td', attrs={'class': 'yfnc_h', 'nowrap': ''})
+	for y in optionssoup.find_all('td', attrs={'class': 'yfnc_h', 'nowrap': ''})
 ]
 
 # last item in list is gibberish: kill it with fire
@@ -19,3 +20,9 @@ del optionsTable[-1]
 for item in optionsTable:
 	if 'C' not in item[1]:
 		print item
+
+
+# This probably needs to be a function
+# Code to get the "current" stock price
+
+print ("Apple stock price is currently %s." % (ystockquote.get_price('AAPL')))
